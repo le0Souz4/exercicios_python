35a10ed1f42242b5c8221281461a3b94bcb69490
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-11-07 11:21:59 -0300

    Add files via upload

---
+++ exer_02.py
@@ -0,0 +1,8 @@
+#faça um programa que leia o nome de uma pessoa
+#e mostre uma mensagem de boas vindas
+
+nome1 = input("olá, qual o seu nome?")
+print("olá",nome1,"seja bem vindo")
+
+nome2 = input("digite seu sobrenome: ")
+print(f"funcionário,{nome1} {nome2}, cadastrado no sistema")
