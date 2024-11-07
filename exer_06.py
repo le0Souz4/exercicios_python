6e011848d15e46970d3e570b6ce9f347de4c7e61
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-11-07 11:56:26 -0300

    Add files via upload

---
+++ exer_06.py
@@ -0,0 +1,8 @@
+"""desenvolva um programa que leia as notas de um aluno,
+calcule e mostre sua média"""
+
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+nota3 = int(input("Digite a terceira nota: "))
+med = (nota1 + nota2 + nota3)/3
+print(f"a nota final do aluno foi {med}")
