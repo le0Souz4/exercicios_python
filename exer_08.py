d82bf8657333360b69f939b58971c8ee8a817c95
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-11-07 13:29:06 -0300

    Add files via upload

---
+++ exer_08.py
@@ -0,0 +1,17 @@
+"""faça um programa que leia um numero inteiro e mostrea
+na tela a sua tabuada"""
+
+num = int(input("Digite um numero: "))
+print(f"a tabuada de {num} é:"
+      f"\n{num} * 1 = ", num*1,
+      f"\n{num} * 2 = ", num*2,
+      f"\n{num} * 3 = ", num*3,
+      f"\n{num} * 4 = ", num*4,
+      f"\n{num} * 5 = ", num*5,
+      f"\n{num} * 6 = ", num*6,
+      f"\n{num} * 7 = ", num*7,
+      f"\n{num} * 8 = ", num*8,
+      f"\n{num} * 9 = ", num*9,
+      f"\n{num} * 10 = ", num*10,)
+
+#mais pra frente esse exercicio será resolvido envolvendo laços de repetição
