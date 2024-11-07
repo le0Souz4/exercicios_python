b9b815a5584f9f0e7b04e7b6a600318e3158969e
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-11-07 19:05:26 -0300

    Add files via upload

---
+++ exer_12.py
@@ -0,0 +1,5 @@
+"""faça um algoritmo que leia o salario de um funcionário
+e mostre seu novo salário com 15% de aumento"""
+sal1 = float(input("irforme o salario: R$ "))
+sal2 = sal1 + (sal1 * 0.15)
+print(f"o novo salário, com um reajuste de 15%, terá o valor de {sal2}")
