cce71b4376212d5e4e516e2b4f58f110d39d067f
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-11-16 18:14:24 -0300

    Add files via upload

---
+++ exer_48.py
@@ -0,0 +1,10 @@
+"""utilize o mesmo exercicio 08, mostrando a tabuada
+de um número que o usuário escolher, só que agora
+utilizando o laço for"""
+num = int(input("digite um número: "))
+for tabuada in range(1, 11):
+    print(f"{num} x {tabuada} = {num * tabuada}")
+
+
+
+
