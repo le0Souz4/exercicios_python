3f68324fb725bcec280a831deb1e11fc63432685
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-11-22 20:28:35 -0300

    Update exer_16.py

--- exer_16.py
+++ exer_16.py
@@ -3,7 +3,14 @@ Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente
 de um triângulo retângulo. Calcule e mostre o comprimento da hipotenusa.
 """
 
+import math
+
 cat_opo = float(input('digite o comprimento do cateto oposto: '))
 cat_adj = float(input('agora digite o comprimento do cateto adjacente: '))
-hipo = cat_opo**2 + cat_adj**2
-print(f'por meio do teorema de pitágoras, o comprimento da hipotenusa será de {hipo**(1/2):.2f}')
+
+# este seria o jeito mais dificil de se fazer
+# hipo = cat_opo**2 + cat_adj**2
+
+hipo = math.hypot(cat_opo, cat_adj)
+
+print(f'por meio do teorema de pitágoras, o comprimento da hipotenusa será de {hipo:.2f}')
