c8ae68ebe70a19a9d0db13872891785582d4d106
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-12-19 19:26:44 -0300

    Função de Contador

---
+++ exer_97.py
@@ -0,0 +1,8 @@
+"""
+Faça um programa que tenha uma função chamada contador(),
+que receba tres parametros: inicio, fim e passo. seu programa
+tem que realizar tres contagens atraves da função criada:
+a >> de 1 até 10, de 1 em 1
+b >> de 10 até 0, de 2 em 2
+c >> uma contagem personalizada
+"""
