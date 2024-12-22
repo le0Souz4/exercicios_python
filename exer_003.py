4814e9353ae8f944c4651cbb448fc220c7111312
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-12-22 18:01:41 -0300

    Somando dois números

---
+++ exer_003.py
@@ -0,0 +1,14 @@
+#Crie um programa que leia dois numeros
+#e mostre a soma entre eles
+
+# Solicita ao usuário que digite o primeiro número e converte para inteiro
+num1 = int(input("digite o primeiro numero: "))
+
+# Solicita ao usuário que digite o segundo número e converte para inteiro
+num2 = int(input("digite o segundo numero: "))
+
+# Realiza a soma dos dois números fornecidos pelo usuário
+soma = num1 + num2
+
+# Exibe o resultado da soma, mostrando os dois números e o valor total
+print(f"a soma entre {num1} e {num2} vale {soma}")
--- exer_03.py
+++
@@ -1,7 +0,0 @@
-#Crie um programa que leia dois numeros
-#e mostre a soma entre eles
-
-num1 = int(input("digite o primeiro numero: "))
-num2 = int(input("digite o segundo numero: "))
-soma = num1 + num2
-print(f"a soma entre {num1} e {num2} vale {soma}")
