e31ef920b0f2e4a043090c5af554413e99ee7ed3
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-12-21 22:17:31 -0300

    Respondendo ao Usuário 2

--- exer_02.py
+++ exer_02.py
@@ -6,3 +6,12 @@ print("olá",nome1,"seja bem vindo")
 
 nome2 = input("digite seu sobrenome: ")
 print(f"funcionário,{nome1} {nome2}, cadastrado no sistema")
+
+"""
+Aqui vemos o uso da função input() que permitirá o usuário digitar algo, e poder 
+atribuir o valor digitado à veriável, nesse caso, nome1 e nome2.
+Temos Tambem dois exemplos de uso da função print() só que agora inserindo as variáveis
+para que elas tambem sejam mostradas. 
+No primeiro caso temos a variável isolada, e no segundo usando o comando f"" para avisar
+a função que queremos concatenar as variáveis ao texto, colocando elas entre chaves {}
+"""
