8fc805c2ffec4e1b162d09f10b89880b5cb7f664
Leonardo Souza <105828443+le0Souz4@users.noreply.github.com>
2024-12-21 22:12:04 -0300

    Respondendo ao Usuário

--- exer_01.py
+++ exer_01.py
@@ -2,3 +2,9 @@
 print("olá mundo!")
 msg = "olá mundo!"
 print(msg)
+
+""" exemplo básico do uso da função print() que irá imprimir o valor 
+digitado na tela do usuário, e tambem de atribuição de um valor a
+uma variável, nesse caso, msg.
+note que olá mundo está entre parênteses, toda 'string' ou cadeia de caracteres
+deve estar entre "" ou ''.
